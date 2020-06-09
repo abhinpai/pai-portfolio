5f0d5aba8c5b844a835406849b101a58c078185a
Abhin Pai <abhinpai96@gmail.com>
2020-06-09 20:58:38 +0530

    Deploy website - based on a885012a6020b8a948e87394dc69f190a16de01b

---
+++ 8ec65dbe.340e3d60.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{124:function(o){o.exports=JSON.parse('{"allTagsPath":"/pai-portfolio/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/pai-portfolio/blog/tags/facebook"}')}}]);
