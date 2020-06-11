344014d61927acff18744b35e3a7b05c31532e4c
Abhin Pai <abhinpai96@gmail.com>
2020-06-12 00:56:05 +0530

    Update remote deployment code 🗃

--- github-deploy.js
+++ github-deploy.js
@@ -1,20 +1,25 @@
 const execSync = require("child_process").execSync;
 
 const repository = "abhinpai.github.io";
-const portfoliorepo = "pai-portfolio";
-const portfolioPath = `/Users/abhinpai/Documents/MyProjects/React/${portfoliorepo}`;
 const dirpath = `/Users/abhinpai/Documents/MyProjects/React/${repository}`;
+
 console.log("🔸Building package 🛠");
-execSync("yarn build", { encoding: "utf-8" })
+execSync("yarn build", { encoding: "utf-8" });
+
+console.log("🔸Deleting previous package 🗑");
+execSync(`rm -rf ${dirpath}/*`, { encoding: "utf-8" }); 
+execSync("git add -A", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+execSync("git commit -m \"Cleaning repository\"", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+execSync("git push -u origin master", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+
 console.log("🔸Copying the build package into abhinpai.github.io 🚚");
-execSync(`cp -r build ${dirpath}`, { encoding: "utf-8" }); 
+execSync(`cp -r build/. ${dirpath}`, { encoding: "utf-8" }); 
 console.log("🔸Package copied successfully ✅");
-console.log(`🔸Changing directory to ${repository}`);
-execSync(`cd ${dirpath}`, { encoding: "utf-8" }); 
+
 console.log(`🔸Publishing the 📦 to ${repository} repository`);
-execSync("git add -A", { encoding: "utf-8" }); 
-const publishmessage = "Publishing package";
-execSync("git commit -m \"Publish package\"", { encoding: "utf-8" }); 
-execSync("git push -u origin master", { encoding: "utf-8" }); 
+// execSync("git init", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+// execSync("git remote add origin https://github.com/abhinpai/abhinpai.github.io.git", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+execSync("git add -A", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+execSync("git commit -m \"Publishing package\"", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
+execSync("git push -u origin master", {cwd: `${dirpath}`}, { encoding: "utf-8" }); 
 console.log(`🥳 Hurry your website is live now 🌏🚀 https://abhinpai.github.io/`);
-execSync(`cd ${portfolioPath}`, { encoding: "utf-8" }); 
