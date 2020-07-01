b535ad552c9dd00d6f5ba78fc6521437bc7d963b
Abhin Pai <abhinpai96@gmail.com>
2020-07-01 22:36:01 +0530

    Updated glowbullet componet to accept list of sting [😃 😄 😁 😆]

--- src/components/glowBullet.jsx
+++ src/components/glowBullet.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-export default function GlowBullet({ children, focusWord, highlightWord }) {
+export default function GlowBullet({
+  children,
+  focusWord,
+  highlightWord,
+  highlightWords,
+}) {
   return (
     <div className={styles.masterDiv}>
       <span className={styles.blob} />
       <h3>
-        {highlightWord && <span className={styles.highlightWord}>{highlightWord}</span>}
+        {highlightWords &&
+          highlightWords.forEach((element) => {
+            <span className={styles.highlightWord}>{element}</span>;
+          })}
+        {highlightWord && (
+          <span className={styles.highlightWord}>{highlightWord}</span>
+        )}
         {focusWord && <span className={styles.focus}>{focusWord}</span>}
-        {children}
+        {children && children}
       </h3>
     </div>
   );
