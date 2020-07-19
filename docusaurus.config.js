f3ec2a21b329acbb07c75cfb02362f244f4b9a87
Abhin Pai <abhinpai96@gmail.com>
2020-07-19 20:19:10 +0530

    Renamed the path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: "Abhin Pai",
+  title: 'Abhin Pai',
   tagline: "Having a strong belief that Programming doesn't have any Language",
-  url: "https://abhinpai.github.io",
-  baseUrl: "/",
-  favicon: "img/favicon.ico",
-  organizationName: "abhinpai",
-  projectName: "pai-portfolio",
+  url: 'https://abhinpai.github.io',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'abhinpai',
+  projectName: 'pai-portfolio',
   themeConfig: {
     defaultDarkMode: true,
     sidebarCollapsible: true,
     googleAnalytics: {
-      trackingID: "G-61TLB4TLQ6",
+      trackingID: 'G-61TLB4TLQ6',
     },
     gtag: {
-      trackingID: "G-61TLB4TLQ6",
+      trackingID: 'G-61TLB4TLQ6',
     },
     announcementBar: {
-      id: "underconstruction",
-      content: "⭐️ This website is under construction  ⭐️",
+      id: 'underconstruction',
+      content: '⭐️ This website is under construction  ⭐️',
     },
     prism: {
-      theme: require("prism-react-renderer/themes/github"),
-      darkTheme: require("prism-react-renderer/themes/dracula"),
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
     },
     algolia: {
       apiKey: '182fa59875da31eb97b079236300f6ed',
@@ -30,18 +30,18 @@ module.exports = {
     navbar: {
       hideOnScroll: false,
       logo: {
-        alt: "My Site Logo",
-        src: "img/logo.png",
+        alt: 'My Site Logo',
+        src: 'img/logo.png',
       },
       links: [
         {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "Technical Notes",
-          position: "right",
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Technical Notes',
+          position: 'right',
         },
-        { to: "blog", label: "Blogs", position: "right" },
-        { to: "about/about", label: "About Me", position: "right" },
+        { to: 'blog', label: 'Blogs', position: 'right' },
+        { to: 'about/Abhin-Pai', label: 'About Me', position: 'right' },
         {
           href: 'https://github.com/abhinpai',
           position: 'right',
@@ -50,18 +50,18 @@ module.exports = {
         },
       ],
     },
-    footer:{
+    footer: {
       // style: 'dark',
       copyright: `Copyright © ${new Date().getFullYear()}, Abhin Pai`,
-    }
+    },
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          homePageId: "introduction",
-          sidebarPath: require.resolve("./sidebars.js"),
+          homePageId: 'introduction',
+          sidebarPath: require.resolve('./sidebars.js'),
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
@@ -69,7 +69,7 @@ module.exports = {
           showReadingTime: true,
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
