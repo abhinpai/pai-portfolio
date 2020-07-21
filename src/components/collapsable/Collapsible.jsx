88c9d854c27bd86283cca3e19d52f97b404ed3a5
Pai <abhin.pai@honeywell.com>
2020-07-21 13:28:51 +0530

    Updated react-transition lib

--- src/components/collapsable/Collapsible.jsx
+++ src/components/collapsable/Collapsible.jsx
@@ -1,38 +1,53 @@
 import React, { useState } from "react";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./Collapsible.css";
 
 const Collapsible = ({ children, title }) => {
   const [collapse, setCollapse] = useState(false);
-
   return (
     <div>
       <div onClick={() => setCollapse(!collapse)} className="header">
         {title != null ? title : "Answer"}
       </div>
+
       <Toggle hidden={collapse}>{children}</Toggle>
     </div>
   );
 };
 
-export class Toggle extends React.Component {
-  render() {
-    return (
-      <div>
-        <ReactCSSTransitionGroup
-          transitionName="toggle"
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={300}
-        >
-          {this.props.hidden ? (
+export const Toggle = ({ hidden, children }) => {
+  return (
+    <div>
+      <TransitionGroup>
+        {hidden ? (
+          <CSSTransition
+            classNames="fade"
+            timeout={500}
+          >
             <div key={"answer"} className="toggle-base">
-              {this.props.children}
+              {children}
             </div>
-          ) : null}
-        </ReactCSSTransitionGroup>
-      </div>
-    );
-  }
-}
+          </CSSTransition>
+        ) : (
+          <div />
+        )}
+      </TransitionGroup>
+    </div>
+  );
+};
 
 export default Collapsible;
+
+
+{/* <ReactCSSTransitionGroup
+transitionName="toggle"
+transitionEnterTimeout={300}
+transitionLeaveTimeout={300}
+>
+{this.props.hidden ? (
+  <div key={"answer"} className="toggle-base">
+    {this.props.children}
+  </div>
+) : null}
+</ReactCSSTransitionGroup> */}
